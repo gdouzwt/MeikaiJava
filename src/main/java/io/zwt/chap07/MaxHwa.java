4990b5e55161e5616c0433a47faf46c64e70a1a7
zhaowentao <gdouzwt@gmail.com>
2019-03-21 17:28:53 +0800

    add MaxHwa.java

---
+++ src/main/java/io/zwt/chap07/MaxHwa.java
@@ -0,0 +1,41 @@
+package io.zwt.chap07;
+
+import java.util.Scanner;
+
+public class MaxHwa {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int[] height = new int[3];
+        int[] weight = new int[3];
+        int[] age    = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("[" + (i + 1) + "] ");
+            System.out.print("身高：");  height[i] = stdIn.nextInt();
+            System.out.print("体重：");  weight[i] = stdIn.nextInt();
+            System.out.print("年龄：");  age[i]    = stdIn.nextInt();
+        }
+
+        int maxHeight = height[0];
+        if (height[1] > maxHeight) maxHeight = height[1];
+        if (height[2] > maxHeight) maxHeight = height[2];
+
+        int maxWeight = weight[0];
+        if (weight[1] > maxWeight) maxWeight = weight[1];
+        if (weight[2] > maxWeight) maxWeight = weight[2];
+
+        int maxAge = age[0];
+        if (age[1] > maxAge) maxAge = age[1];
+        if (age[2] > maxAge) maxAge = age[2];
+
+        System.out.println("身高的最大值为：" + maxHeight + "。");
+        System.out.println("体重的最大值为：" + maxWeight + "。");
+        System.out.println("年龄的最大值为：" + maxAge + "。");
+
+
+
+
+
+    }
+}
