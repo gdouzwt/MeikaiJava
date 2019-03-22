9db5eb67622bd92fd75a3a231307d28acb640bc0
zhaowentao <gdouzwt@hotmail.com>
2019-03-22 15:35:11 +0800

    更新了Power.java 已完善

--- src/main/java/io/zwt/chap07/Power.java
+++ src/main/java/io/zwt/chap07/Power.java
@@ -8,7 +8,7 @@ public class Power {
     static double power(double x, int n) {
         double tmp = 1.0;
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             tmp *= x; // 将tmp乘以x
         }
         return tmp;
@@ -17,7 +17,10 @@ public class Power {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
+        System.out.println("计算a的b次幂。");
+        System.out.print("实数a：");  double a = stdIn.nextDouble();
+        System.out.print("整数b：");  int b = stdIn.nextInt();
 
-
+        System.out.println(a + "的" + b + "次幂为" + power(a, b) + "。");
     }
 }
