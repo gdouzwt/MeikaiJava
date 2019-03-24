565492105d57be267b2f2e0ead485cfcfe6a5e22
zhaowentao <gdouzwt@hotmail.com>
2019-03-24 16:03:02 +0800

    完成练习7-10

--- src/main/java/io/zwt/chap07/exe/Exe10.java
+++ src/main/java/io/zwt/chap07/exe/Exe10.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
  * Extension to MentalArithmetic
  */
 public class Exe10 {
-    static Scanner stdIn = new Scanner(System.in);
+    private static Scanner stdIn = new Scanner(System.in);
 
-    static boolean confirmRetry() {
+    private static boolean confirmRetry() {
         int cont;
         do {
-            System.out.print("再来一次？<Yes~1/No~0>: ");
+            System.out.print("再来一次？ <Yes~1/No~0>: ");
             cont = stdIn.nextInt();
         } while (cont != 0 && cont != 1);
         return cont == 1;
@@ -28,20 +28,61 @@ public class Exe10 {
             int x = rand.nextInt(900) + 100;  // 3位数
             int y = rand.nextInt(900) + 100;  // 3位数
             int z = rand.nextInt(900) + 100;  // 3位数
-            while (true) {
+            int c = rand.nextInt(4);
 
-                if (questions(x, y, z)) break;
+            switch (c) {
+                case 0:
+                    case0IsCorrect(x, y, z); break;
+                case 1:
+                    case1IsCorrect(x, y, z); break;
+                case 2:
+                    case2IsCorrect(x, y, z); break;
+                case 3:
+                    case3IsCorrect(x, y, z); break;
+                default:
+                    break;
             }
+
         } while (confirmRetry());
     }
 
-    private static boolean questions(int x, int y, int z) {
-        System.out.print(x + " + " + y + " + " + z + " = ");
-        int k = stdIn.nextInt();
-        if (k == x + y + z)
-            return true;
-        System.out.println("回答错误！！");
-        return false;
+    private static void case0IsCorrect(int x, int y, int z) {
+        while (true) {
+            System.out.print(x + " + " + y + " + " + z + " = ");
+            int k = stdIn.nextInt();
+            if (k == x + y + z)
+                break;
+            System.out.println("回答错误！!！");
+        }
     }
 
+    private static void case1IsCorrect(int x, int y, int z) {
+        while (true) {
+            System.out.print(x + " + " + y + " - " + z + " = ");
+            int k = stdIn.nextInt();
+            if (k == x + y - z)
+                break;
+            System.out.println("回答错误！!！");
+        }
+    }
+
+    private static void case2IsCorrect(int x, int y, int z) {
+        while (true) {
+            System.out.print(x + " - " + y + " + " + z + " = ");
+            int k = stdIn.nextInt();
+            if (k == x - y + z)
+                break;
+            System.out.println("回答错误！!！");
+        }
+    }
+
+    private static void case3IsCorrect(int x, int y, int z) {
+        while (true) {
+            System.out.print(x + " - " + y + " - " + z + " = ");
+            int k = stdIn.nextInt();
+            if (k == x - y - z)
+                break;
+            System.out.println("回答错误！!！");
+        }
+    }
 }
